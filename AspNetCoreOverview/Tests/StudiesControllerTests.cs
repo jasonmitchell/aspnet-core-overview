438158e98c850e3027a7112abc77583c45e8bca7
Jason Mitchell <jasonmitchell@live.co.uk>
2018-07-23 16:53:44 +0100

    api and tests

---
+++ AspNetCoreOverview/Tests/StudiesControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using Web.Api;
+using Web.Api.Controllers;
+using Xunit;
+
+namespace Tests
+{
+    public class StudiesControllerTests
+    {
+        private readonly TestServer _server;
+
+        public StudiesControllerTests()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+        }
+
+        [Fact]
+        public async Task List_ReturnsArrayOfStudies()
+        {
+            var client = _server.CreateClient();
+            var response = await client.GetAsync("/studies");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var body = await response.Content.ReadAsStringAsync();
+            var studies = JsonConvert.DeserializeObject<Study[]>(body);
+
+            studies.Should().BeEquivalentTo(new[]
+            {
+                new Study {Id = 1, Title = "My Study"},
+                new Study {Id = 2, Title = "Another Study"}
+            });
+        }
+    }
+}
---
+++ AspNetCoreOverview/Web.Api/Controllers/StudiesController.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class StudiesController : ControllerBase
+    {
+        public ActionResult<Study[]> List()
+        {
+            return new[]
+            {
+                new Study {Id = 1, Title = "My Study"},
+                new Study {Id = 2, Title = "Another Study"}
+            };
+        }
+
+        [Route("{id}")]
+        public ActionResult<Study> Get(int id)
+        {
+            return new Study {Id = id, Title = "string"};
+        }
+
+        [Route("{id}/slides")]
+        public ActionResult<Slide[]> GetSlides(int id)
+        {
+            return new[] {new Slide()};
+        }
+
+        [HttpPost]
+        public ActionResult Create(StartStudy command)
+        {
+            // call application layer
+            return Created("/studies/123", new { Id = 123});
+        }
+    }
+
+    public class StartStudy
+    {
+        public string Title { get; set; }
+    }
+
+    public class Slide  
+    {
+    }
+
+    public class Study
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
